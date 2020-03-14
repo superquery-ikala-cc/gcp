90610f036cd7036ae1e6bd2bce97a57ca5b54006
CC <jim.lin@ikala.tv>
2020-03-14 16:39:36 +0800

    add lab-spanner-til (in progress)

---
+++ lab-spanner-til/startup-script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# install docker
+apt-get update
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
+docker run hello-world
+
+# install kubectl
+
+apt-get update 
+
+apt-get install -y apt-transport-https
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+
+apt-get install kubectl -y
