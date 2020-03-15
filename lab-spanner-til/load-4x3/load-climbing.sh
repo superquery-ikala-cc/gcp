47c9d5f580f1ad3a313b88fe4f7cdc2d3042da03
CC <jim.lin@ikala.tv>
2020-03-15 19:55:48 +0800

    add lab-spanner-til/load-4x3

--- lab-spanner-til/load-4x3/load-climbing.sh
+++ lab-spanner-til/load-4x3/load-climbing.sh
@@ -2,7 +2,7 @@
 
 set -e
  
-echo "$(date) | CC | start load-4x3"
+echo "$(date) | CC | start load-climbing"
 
 function generate() {
     sudo docker run --env-file config-02-v1.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-02:v1 generate
@@ -21,20 +21,20 @@ function create() {
 # create
 
 function load() {
-    for case in 02 03
+    for case in 02
     do
-        for version in v1 v2
+        for version in v1
         do
-            for round in {1..3}
+            for ccu in 10 20 40 80 160
             do
                 echo "$(date) | CC | reset ${case}-${version}"
                 sudo docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} reset
 		echo "$(date) | CC | sleep 60s ..."
 		sleep 60
 
-                echo "$(date) | CC | start ${case}-${version} round ${round}"
+                echo "$(date) | CC | start ${case}-${version} ccu ${ccu}"
                 sudo docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} load
-                echo "$(date) | CC | end ${case}-${version} round ${round}"
+                echo "$(date) | CC | end ${case}-${version} ccu ${ccu}"
 	        echo "$(date) | CC | sleep 240s ..."
 	        sleep 240
             done
@@ -45,4 +45,4 @@ function load() {
 
 $1
 
-echo "$(date) | CC | end load-4x3"
+echo "$(date) | CC | end load-climbing"
