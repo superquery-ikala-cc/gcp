cf276f147b8eacd5df81fdf1eea0c32f20db8586
CC <jim.lin@ikala.tv>
2020-03-15 13:43:27 +0800

    add lab-spanner-til

--- lab-spanner-til/load-4x3/load-4x3.sh
+++ lab-spanner-til/load-4x3/load-4x3.sh
@@ -6,11 +6,15 @@ for case in 02 03
 do
     for version in v1 v2
     do
+        sudo docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} create
+        sudo docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} generate
+
         for round in {1..3}
         do
             echo "$(date) | CC | ${case}-${version} round ${round}"
-            docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} load
-            docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} reset
+            sudo docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} load
+            sudo docker run --env-file config-${case}-${version}.env -it gcr.io/gcp-expert-sandbox-jim/til-about-cloudspanner-${case}:${version} reset
+	    echo "$(date) | CC | sleeping 300s ..."
 	    sleep 300
         done
     done
